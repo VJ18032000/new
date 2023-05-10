d29238e9e21b01f76c82e2ee823fa5431d669dbb
vijayakumar <86522987+VJ18032000@users.noreply.github.com>
2023-05-10 15:01:17 +0530

    Update profile.js

--- router/Pos_billing/profile.js
+++ router/Pos_billing/profile.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const upload=require('../../middleware/pos_billing_profile')
 const profileController = require('../../controller/Pos_billing/profile')
 
-router.get('/profile',upload.single('profile_image'),profileController.register)
+router.post('/profile',upload.single('profile_image'),profileController.register)
 
 
 
